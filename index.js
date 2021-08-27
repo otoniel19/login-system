eb1fcccfc4f1845ff251b868e110a735cdd6f2ae
otoniel19 <otonielcostaoliveira3@gmail.com>
2021-08-27 14:07:20 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,84 +1,96 @@
 const fs = require('fs')
-var path = "./src/banco.json"
 
-var readlineSync = require('readline-sync');
+function starts() {
 
-var tipo = readlineSync.question("entrar registrar ou ver dados?: ")
+  var path = "./src/banco.json"
 
-if (tipo === "registrar") {
+  var readlineSync = require('readline-sync');
 
-  var nome = readlineSync.question("seu nome: ")
-  var idade = readlineSync.question("sua idade: ")
-  var continuar = readlineSync.question("continuar? s / n: ")
+  var tipo = readlineSync.question("entrar registrar ver dados ou sair?: ")
 
-  if (continuar === "s") {
+  if (tipo === "registrar") {
 
-    try {
+    var nome = readlineSync.question("seu nome: ")
+    var idade = readlineSync.question("sua idade: ")
+    var continuar = readlineSync.question("continuar? s / n: ")
 
-      var obj = {
-        name: nome,
-        age: idade
+    if (continuar === "s") {
+
+      try {
+
+        var obj = {
+          name: nome,
+          age: idade
+        }
+
+        const banco = JSON.parse(fs.readFileSync(path))
+        fs.writeFileSync(path, JSON.stringify(obj))
+
+      } catch (e) {
+        if (e) {
+          fs.writeFileSync(path, "{}")
+        }
       }
 
-      const banco = JSON.parse(fs.readFileSync(path))
-      fs.writeFileSync(path, JSON.stringify(obj))
+    } else {
+      console.log("Operação terminada...");
+    }
 
-      setTimeout(() => {
-        console.log(banco);
-      }, 3000)
+  } else if (tipo === "entrar") {
+    try {
+
+      const pegaDadosBanco = JSON.parse(fs.readFileSync(path))
+
+      if (pegaDadosBanco.name && pegaDadosBanco.age) {
+
+        var Lognome = readlineSync.question("seu nome: ")
+        if (Lognome === pegaDadosBanco.name) {
+          console.log("Ok")
+        } else {
+          console.log("Esse nome não existe!")
+          while (Lognome != pegaDadosBanco.name) {
+            Lognome = readlineSync.question("seu nome:")
+          }
+        }
+
+        var Logidade = readlineSync.question("sua idade: ")
+        if (Logidade === pegaDadosBanco.age) {
+          console.log("Ok")
+        } else {
+          console.log("Idade Inválida!")
+          while (Logidade != pegaDadosBanco.age) {
+            Logidade = readlineSync.question("sua idade: ")
+          }
+        }
+
+        console.log("Entrou com sucesso!")
+
+      } else {
+        console.log("Não a dados no banco!")
+      }
 
     } catch (e) {
       if (e) {
         fs.writeFileSync(path, "{}")
       }
     }
+  } else if (tipo === "ver dados") {
+    console.clear()
+    const verDados = JSON.parse(fs.readFileSync(path))
+    console.log("Seus dados são: \n")
+    console.log(verDados)
+    console.log("\n")
+  } else if(tipo === "sair") {
+    console.log("Obrigado por testar!")
+  }
 
+  var sair = readlineSync.question("deseja executar novamente? s / n: ")
+ 
+  if (sair === "s") {
+    starts()
   } else {
-    console.log("Operação terminada...");
+    console.log("Obrigado por testar!")
   }
 
-} else if (tipo === "entrar") {
-  try {
-
-    const pegaDadosBanco = JSON.parse(fs.readFileSync(path))
-
-    if (pegaDadosBanco.name && pegaDadosBanco.age) {
-
-      var Lognome = readlineSync.question("seu nome: ")
-      if (Lognome === pegaDadosBanco.name) {
-        console.log("Ok")
-      } else {
-        console.log("Esse nome não existe!")
-        while (Lognome != pegaDadosBanco.name) {
-          Lognome = readlineSync.question("seu nome:")
-        }
-      }
-
-      var Logidade = readlineSync.question("sua idade: ")
-      if (Logidade === pegaDadosBanco.age) {
-        console.log("Ok")
-      } else {
-        console.log("Idade Inválida!")
-        while (Logidade != pegaDadosBanco.age) {
-          Logidade = readlineSync.question("sua idade: ")
-        }
-      }
-
-      console.log("Entrou com sucesso!")
-
-    } else {
-      console.log("Não a dados no banco!")
-    }
-
-  } catch (e) {
-    if (e) {
-      fs.writeFileSync(path, "{}")
-    }
-  }
-} else if(tipo === "ver dados") {
-  console.clear()
-  const verDados = JSON.parse(fs.readFileSync(path))
-  console.log("Seus dados são: \n")
-  console.log(verDados)
-  console.log("\n")
 }
+starts()
